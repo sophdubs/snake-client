52eb8f07919a3fb9179034404e68bc80c9f470e9
Sophie <sophiemargueritedubois@gmail.com>
2020-06-03 17:35:28 +0000

    Able to move the snake in all 4 didirections and exit the game using command c

---
+++ input.js
@@ -0,0 +1,31 @@
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+const stdin = setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  };
+};
+
+stdin.on('data', key => {
+  handleUserInput(key);
+});
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-connect();
+// connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-const stdin = setupInput();
+setupInput(connect());
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
 
-stdin.on('data', key => {
-  handleUserInput(key);
-});
