43fc4897573616a7de5af6fb5c129d629acab1ba
Sophie <sophiemargueritedubois@gmail.com>
2020-06-03 17:18:21 +0000

    Successfully implemented setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+const stdin = setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
+
+stdin.on('data', key => {
+  handleUserInput(key);
+});
